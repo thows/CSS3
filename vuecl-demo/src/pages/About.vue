753d875971f9d45805804f0937a338e2b6a3f566
lamp <907888614@qq.com>
2020-10-13 13:19:45 +0800

    fix dir

--- vuecl-demo/src/pages/About.vue
+++ vuecl-demo/src/pages/About.vue
@@ -2,4 +2,21 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <button @click="pop()">操作</button>
 </template>
+
+<script>
+    export default {
+      components: {},
+      data() {return {}},
+      computed: {},
+      mounted() {
+      
+      },
+      methods: {
+        pop(){
+          alert("click")
+        }
+      }
+    }
+</script>
